ba9014d3f11c3ce7c22c9d2492bcc01b8ecac4f2
darosior <darosior@gmail.com>
2019-03-16 04:54:34 +0100

    Refactoring

--- contracts/contracts/Organization.sol
+++ contracts/contracts/Organization.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.4.22 <0.6.0;
 import "./Mortal.sol";
 
 contract Organization is Mortal {
-
+    // A member of the organization
     struct Member {
         string firstName;
         string lastName;
@@ -11,24 +11,28 @@ contract Organization is Mortal {
         uint id;
     }
 
-    struct Request {
+    // A request made to add a new member to the organization
+    struct accessRequest {
         string firstName;
         string lastName;
         bool exists;
         uint id;
-        uint acceptedcount;
-        uint rejectedcount;
+        mapping(address => bool) hasVoted; // You wont fool us ><
+        uint acceptedcount; // A counter to avoid a FUCKING BIG GAZ CONSUMING for loop to retrieve the %
     }
 
+    // Organization informations
     string name;
     string id;
     string website;
 
+    // All the members of the org
     mapping(address => Member) members;
-    address[] membersAddress;
+    address[] membersAddresses;
 
-    mapping(address => Request) requests;
-    address[] requestsAddress;
+    // All the membership requests
+    mapping(address => accessRequest) accRequests;
+    address[] accRequestsAddresses;
 
     event NewMembershipRequest(address requester, string firstName, string lastName);
     event VotedForNewMember(address member, address requester, bool decision);
@@ -43,7 +47,13 @@ contract Organization is Mortal {
         name = _name;
         id = _id;
         website = _website;
-        membersAddress.length = 0;
+        membersAddresses.length = 0;
+        // The owner is the first member
+        members[msg.sender].firstName = "Jimmy";
+        members[msg.sender].lastName = "Chambrade";
+        members[msg.sender].exists = true;
+        members[msg.sender].availableTokens = 100;
+        members[msg.sender].id = membersAddresses.push(msg.sender);
     }
 
     // Returns organization informations
@@ -51,48 +61,47 @@ contract Organization is Mortal {
         return (name, id, website, getMembersCount());
     }
 
-    function requestMembership (address _address, string memory _firstName, string memory _lastName) public  returns (string memory) {
-        require(!requests[_address].exists, "A request has already been made for this address");
+    // Request a membership for a given address, name, and surname
+    function requestMembership (address _address, string memory _firstName, string memory _lastName) public {
+        require(!accRequests[_address].exists, "A request has already been made for this address");
         require(!members[_address].exists, "This user is already a member of the organization");
 
-        requests[_address].firstName = _firstName;
-        requests[_address].lastName = _lastName;
-        requests[_address].exists = true;
-        requests[_address].acceptedcount = 0;
-        requests[_address].rejectedcount = 0;
-        requests[_address].id = requestsAddress.push(_address);
+        // Creation of a membership request for this "person"
+        accRequests[_address].firstName = _firstName;
+        accRequests[_address].lastName = _lastName;
+        accRequests[_address].exists = true;
+        accRequests[_address].acceptedcount = 0;
+        accRequests[_address].id = accRequestsAddresses.push(_address);
 
         emit NewMembershipRequest(_address, _firstName, _lastName);
-        return requests[_address].firstName;
     }
 
-    // Add a new member
-    function addMember(address _address, string memory _firstName, string memory _lastName) public onlyMember returns (string memory) {
-        require(!members[_address].exists, "User already exists.");
-
-        members[_address].firstName = _firstName;
-        members[_address].lastName = _lastName;
+    // Adds a member to the list given a membership request
+    function addMember(accessRequest memory request, address _address) private returns (bool) {
+        members[_address].firstName = request.firstName;
+        members[_address].lastName = request.lastName;
         members[_address].exists = true;
         members[_address].availableTokens = 100;
-        members[_address].id = membersAddress.push(_address);
-
-        return members[_address].firstName;
+        members[_address].id = membersAddresses.push(_address);
+        
+        return true;
     }
 
-    function acceptNewMembership(address _requester, bool _decision) public onlyMember returns (bool) {
-        require(!requests[_requester].exists, "Unknown address in requester list");
+    // Vote from a member to accept or decline a membership request
+    function acceptNewMembership(address _requester) public onlyMember {
+        require(!accRequests[_requester].exists, "No request have been made for this address");
+        require(!accRequests[_requester].hasVoted[msg.sender]); // You can't vote twice if you voted yes, otherwise you can switch (you know people do change..)
         
-        if(_decision == true){
-            requests[_requester].acceptedcount ++;
-        } else {
-            requests[_requester].rejectedcount ++;
+        accRequests[_requester].hasVoted[msg.sender] = true; // This address won't be able to vote anymore
+        ++accRequests[_requester].acceptedcount; // __*Then*__ we increase the counter
+        if (accRequests[_requester].acceptedcount > membersAddresses.length / 2) {
+            // If a membership request reaches majority, it is accepted
+            if (!addMember(accRequests[_requester], _requester)) {
+                revert();
+            }
+            delete accRequests[_requester];
+            emit VotedForNewMember(msg.sender, _requester, true);
         }
-
-        emit VotedForNewMember(msg.sender, _requester, _decision);
-
-        // todo check if quorum is reached and add or remove membership
-
-        return _decision;
     }
 
     // Returns member informations
@@ -102,11 +111,11 @@ contract Organization is Mortal {
 
     // Returns member list
     function getMemberList() public view returns (address[] memory) {
-        return membersAddress;
+        return membersAddresses;
     }
 
     // Returns members count
     function getMembersCount() public view returns(uint) {
-        return membersAddress.length;
+        return membersAddresses.length;
     }
 }
